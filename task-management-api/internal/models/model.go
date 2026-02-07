086e472945203c41bee1cf58629b4a4e4f873205
Forester04 <erickforester04@gmail.com>
2026-02-08 00:09:42 +0300

    models: project and task

--- task-management-api/internal/models/model.go
+++ task-management-api/internal/models/model.go
@@ -6,6 +6,14 @@ import (
 	"gorm.io/gorm"
 )
 
+type Status string
+
+const (
+	TODO        Status = "todo"
+	IN_PROGRESS Status = "in_progress"
+	DONE        Status = "done"
+)
+
 type User struct {
 	gorm.Model
 	Email     string `gorm:"type:varchar(100);not null;unique"`
@@ -15,4 +23,23 @@ type User struct {
 	LastName  string `gorm:"type:varchar(100);not null"`
 	Phone     *string
 	BirthDate *time.Time
+	Projects  []*Project
+	Tasks     []*Task
+}
+
+type Project struct {
+	gorm.Model
+	Name        string `gorm:"type:varchar(100);not null"`
+	Description string `gorm:"type:text;not null"`
+	UserID      uint
+	Tasks       []*Task
+}
+
+type Task struct {
+	gorm.Model
+	Title       string `gorm:"type:varchar(150);not null;unique"`
+	Description string `gorm:"type:text;not null"`
+	Status      Status `gorm:"type:status;default:todo"`
+	UserID      uint   `gorm:"unique"`
+	ProjectID   uint   `gorm:"unique"`
 }
