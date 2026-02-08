11d5f5af54a907818443d83079300ea7081dc762
Forester04 <erickforester04@gmail.com>
2026-02-08 13:32:10 +0300

    task repository interface

---
+++ task-management-api/internal/repositories/task.go
@@ -0,0 +1,41 @@
+package repositories
+
+import (
+	"fmt"
+
+	"github.com/forester04/go-backend-projects/task-management-api/internal/errcode"
+	"github.com/forester04/go-backend-projects/task-management-api/internal/models"
+	"gorm.io/gorm"
+)
+
+type TaskRepositoryInterface interface {
+	Create(task *models.Task) error
+	GetByID(id uint) (*models.Task, error)
+	Update(task *models.Task) error
+	Delete(id uint) error
+}
+
+type TaskRepository struct {
+	DB *gorm.DB
+}
+
+func (rpt *TaskRepository) Create(task *models.Task) error {
+	return rpt.DB.Create(task).Error
+}
+
+func (rpt *TaskRepository) GetByID(id uint) (*models.Task, error) {
+	task := &models.Task{}
+	err := rpt.DB.Where("id = ?", id).First(task).Error
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", errcode.ErrDatabase, err)
+	}
+	return task, nil
+}
+
+func (rpt *TaskRepository) Update(task *models.Task) error {
+	return rpt.DB.UpdateColumns(task).Error
+}
+
+func (rpt *TaskRepository) Delete(id uint) error {
+	return rpt.DB.Delete(&models.Project{}, id).Error
+}
