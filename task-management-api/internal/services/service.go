aea881b55deef9219e17dd604556fb0338c1aab0
Forester04 <erickforester04@gmail.com>
2026-02-08 23:43:54 +0300

    service interface update

--- task-management-api/internal/services/service.go
+++ task-management-api/internal/services/service.go
@@ -8,10 +8,22 @@ import (
 )
 
 type ServiceInterface interface {
+	/* Auth */
 	RegisterUser(registerUser *dto.RegisterUser) (*models.User, error)
 	LoginUser(email string, password string) (*models.User, error)
 	formatRegisterUser(registerUser *dto.RegisterUser) (*models.User, error)
 	DeleteUser(id uint) error
+	GenerateToken(user *models.User) (tokenString string, err error)
+
+	/* Task */
+	CreateTask(task *models.Task) error
+	GetTask(id uint) (*models.Task, error)
+	DeleteTask(id uint) error
+
+	/* Project */
+	CreateProject(project *models.Project) error
+	GetProject(id uint) (*models.Project, error)
+	DeleteProject(id uint) error
 }
 
 type Service struct {
