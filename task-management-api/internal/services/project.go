6c14ec1a792b0e70f5cf8ab44e70882818c77dc0
Forester04 <erickforester04@gmail.com>
2026-02-08 23:41:43 +0300

    project service

---
+++ task-management-api/internal/services/project.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/forester04/go-backend-projects/task-management-api/internal/errcode"
+	"github.com/forester04/go-backend-projects/task-management-api/internal/models"
+)
+
+func (svc *Service) CreateProject(project *models.Project) error {
+	err := svc.globalRepository.Project.Create(project)
+	if err != nil {
+		return fmt.Errorf("%w: %v", errcode.ErrDatabase, err)
+	}
+	return nil
+}
+
+func (svc *Service) GetProject(id uint) (*models.Project, error) {
+	project, err := svc.globalRepository.Project.GetByID(id)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", errcode.ErrDatabase, err)
+	}
+
+	if project.ID == 0 {
+		return nil, fmt.Errorf("%w: %v", errcode.ErrNotFound, err)
+	}
+	return project, nil
+}
+
+func (svc *Service) DeleteProject(id uint) error {
+	err := svc.globalRepository.Project.Delete(id)
+	if err != nil {
+		return fmt.Errorf("%w: %v", errcode.ErrDatabase, err)
+	}
+	return nil
+}
