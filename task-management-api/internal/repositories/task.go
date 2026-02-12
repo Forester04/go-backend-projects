5cc0ddb91aa79bc427c03d6490cc0609d887b6e7
Forester04 <erickforester04@gmail.com>
2026-02-12 12:00:09 +0300

    updating task repository

--- task-management-api/internal/repositories/task.go
+++ task-management-api/internal/repositories/task.go
@@ -1,72 +1,49 @@
 package repositories
 
 import (
-	"fmt"
-
-	"github.com/forester04/go-backend-projects/task-management-api/internal/errcode"
 	"github.com/forester04/go-backend-projects/task-management-api/internal/models"
 	"gorm.io/gorm"
 )
 
 type TaskRepositoryInterface interface {
-	Create(task *models.Task) error
-	GetByID(id uint) (*models.Task, error)
-	GetByTitle(name string) (*models.Task, error)
-	GetAll() ([]*models.Task, error)
-	Rename(title string, id uint) (*models.Task, error)
-	Update(task *models.Task) error
-	Delete(id uint) error
+	Create(userID uint, projectID uint, task *models.Task) error
+	GetByID(userID uint, projectID uint, id uint) (*models.Task, error)
+	GetByTitle(userID uint, projectID uint, title string) (*models.Task, error)
+	ListByUserProject(userID uint, projectID uint) ([]*models.Task, error)
+	Save(userID uint, projectID uint, task *models.Task) error
+	Delete(userID uint, projectID uint, id uint) error
 }
 
 type TaskRepository struct {
 	DB *gorm.DB
 }
 
-func (rpt *TaskRepository) Create(task *models.Task) error {
-	return rpt.DB.Create(task).Error
+func (rpt *TaskRepository) Create(userID uint, projectID uint, task *models.Task) error {
+	return rpt.DB.Where("user_id = ? AND project_id = ?", userID, projectID).Create(task).Error
 }
 
-func (rpt *TaskRepository) GetByID(id uint) (*models.Task, error) {
+func (rpt *TaskRepository) GetByID(userID uint, projectID uint, id uint) (*models.Task, error) {
 	task := &models.Task{}
-	err := rpt.DB.Where("id = ?", id).First(task).Error
-	if err != nil {
-		return nil, fmt.Errorf("%w: %v", errcode.ErrDatabase, err)
-	}
-	return task, nil
+	err := rpt.DB.Where("user_id = ? AND project_id = ?", userID, projectID).First(&task, id).Error
+	return task, err
 }
 
-func (rpt *TaskRepository) GetByTitle(name string) (*models.Task, error) {
+func (rpt *TaskRepository) GetByTitle(userID uint, projectID uint, title string) (*models.Task, error) {
 	task := &models.Task{}
-	err := rpt.DB.Where("name = ?", name).Limit(1).Find(task)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %v", errcode.ErrDatabase, err)
-	}
-	return task, nil
+	err := rpt.DB.Where("user_id = ? AND project_id = ? AND title = ?", userID, projectID, title).First(&task).Error
+	return task, err
 }
 
-func (rpt *TaskRepository) GetAll() ([]*models.Task, error) {
+func (rpt *TaskRepository) ListByUserProject(userID uint, projectID uint) ([]*models.Task, error) {
 	tasks := []*models.Task{}
-	err := rpt.DB.Find(&tasks)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %v", errcode.ErrDatabase, err)
-	}
-	return tasks, nil
+	err := rpt.DB.Where("user_id = ? AND project_id = ?", userID, projectID).Find(&tasks).Error
+	return tasks, err
 }
 
-func (rpt *TaskRepository) Rename(title string, id uint) (*models.Task, error) {
-	task, err := rpt.GetByID(id)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %v", errcode.ErrDatabase, err)
-	}
-	task.Title = title
-
-	return task, nil
+func (rpt *TaskRepository) Save(userID uint, projectID uint, task *models.Task) error {
+	return rpt.DB.Where("user_id = ? AND project_id = ?", userID, projectID).Save(task).Error
 }
 
-func (rpt *TaskRepository) Update(task *models.Task) error {
-	return rpt.DB.UpdateColumns(task).Error
-}
-
-func (rpt *TaskRepository) Delete(id uint) error {
-	return rpt.DB.Delete(&models.Project{}, id).Error
+func (rpt *TaskRepository) Delete(userID uint, projectID uint, id uint) error {
+	return rpt.DB.Where("user_id = ? AND project_id = ? AND id = ?", userID, projectID, id).Delete(&models.Task{}).Error
 }
